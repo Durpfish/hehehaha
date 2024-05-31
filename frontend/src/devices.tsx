8e66338bf5936ab1ce758b4875e32927b6ecb110
yuu-chennn <yuchen.shen02@gmail.com>
2024-06-01 02:19:25 +0800

    add device vulnerability alert and modal

--- frontend/src/devices.tsx
+++ frontend/src/devices.tsx
@@ -1,10 +1,9 @@
 import './devices.css';
 import 'antd/dist/reset.css';
-import { Table, Button } from 'antd';
+import { Table, Button, Alert, Space, Modal} from 'antd';
 import { useNavigate } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import {sampleDevices1, sampleDevicesColumns } from './data';
-
 
 const Devices = () => {
   const navigate = useNavigate();
@@ -17,6 +16,25 @@ const Devices = () => {
     navigate('/catalog');
   }
 
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const handleOk = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setOpen(false);
+    }, 3000);
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
   // TODO implement a proper backend
   const devices = sampleDevices1
   const columns = sampleDevicesColumns
@@ -25,6 +43,56 @@ const Devices = () => {
     <div className="mainContainer">
       <div className="titleContainer">
         <div>My Devices</div>
+      </div>
+      <div className="alertContainer">
+        <Alert
+          message="Warning"
+          description="New vulnerability detected."
+          type="warning"
+          showIcon
+          action={
+            <Space direction="vertical">
+              <Button type="primary" onClick={showModal}>
+                More
+              </Button>
+              <Modal
+                open={open}
+                title="Security Alert: Critical vulnerabilities"
+                onOk={handleOk}
+                onCancel={handleCancel}
+                footer={[
+                  <Button key="back" onClick={handleCancel}>
+                    Cancel
+                  </Button>,
+                  <Button
+                    key="link"
+                    href="https://google.com"
+                    type="primary"
+                    loading={loading}
+                    onClick={handleOk}
+                  >
+          
+                    Update Device
+                  </Button>,
+                  <Button key="submit" type="primary" onClick={onCatalogButtonClick}>
+                    Browse Catalog
+                  </Button>,
+                ]}
+              >
+                <p><b>What has happened?</b></p>
+                <p>Critical security vulnerabilities in Internet-based medical devices are being exploited actively by cyber attackers. This could potentially lead to system compromise and cause threatening complications if targeted by malicious activity.</p>
+                <p><b>Which devices are affected?</b></p>
+                <ul>
+                  <li>Brand B SN002 Blood Pressure Meter - prior to version 125.0.6422.112</li>
+                  <li>Brand C SN003 Artificial Heart Valve - prior to version 125.0.2535.67</li>
+                </ul>
+                <p><b>What do I need to do?</b></p>
+                <p>Ensure that your device has been updated to the latest version. Alternatively, browse our Device Catalog for a replacement.</p>
+              </Modal>
+            </Space>
+          }
+          closable
+        ></Alert>
       </div>
       <div className="devicesTableContainer">
         <Table
