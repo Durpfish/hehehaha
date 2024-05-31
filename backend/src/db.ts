d1b186bfd1fde472c8a8f1b394949005215bd055
tjingsheng <tanjs41200@gmail.com>
2024-06-01 04:19:16 +0800

    try out env

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,10 +1,15 @@
 import { Pool } from 'pg';
+import * as dotenv from "dotenv";
 
-const pool = new Pool({
-  user:'postgres',
-  host: 'localhost',
-  database: 'synergy_prod',
-  port: 5432,
-});
+dotenv.config();
+
+const dbConfig = {
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432'), // Default port if not specified
+  user: process.env.DB_USER!,
+  database: process.env.DB_NAME!
+};
+
+const pool = new Pool(dbConfig );
 
 export default pool;
